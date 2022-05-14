12f73874f69d5aa79c1300d273f45e1498dafb67
leonardo q <leonardoq.leao@outlook.com>
2022-05-14 15:50:47 -0300

    chore: add all dependecies to react

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import { Component } from "react";
+
+export default class Home extends Component {
+  render(){
+    return (
+      <div className="Home">
+        <h1>
+          asdasdasd
+        </h1>
+      </div>
+    )
+  }
+}
