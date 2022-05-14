1e9d04e4157a5d9b20fe5883842ca0219f1ad8ae
leonardo q <leonardoq.leao@outlook.com>
2022-05-14 16:47:02 -0300

    chore: add root font

--- src/content/Navbar/Navbar.jsx
+++ src/content/Navbar/Navbar.jsx
@@ -1,7 +1,22 @@
-export default function Navbar(){
+export default function Navbar() {
     return (
-        <h1>
-            bom dia
-        </h1>
+        <div className="Navbar">
+            <div className="navbarLeft">
+                <nav>
+                    <ul>
+                        <li>Dashboard</li>
+                        <li>Leads</li>
+                        <li>Campanhas</li>
+                        <li>Vendas</li>
+                        <li>Clientes</li>
+                        <li>Cobrança</li>
+                        <li>Configurações</li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="navbarTop">
+
+            </div>
+        </div>
     )
 }
--- src/css/index.js
+++ src/css/index.js
@@ -1,2 +1,3 @@
 export * from './index.css'
 export * from './home.css'
+export * from '../content/Navbar/css/navbar.css'
