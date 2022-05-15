10aba8b02e7fd2ee984128b20baa20407e866405
leonardo q <leonardoq.leao@outlook.com>
2022-05-15 09:05:52 -0300

    style: referential color in navbar blocks

--- src/content/Navbar/Navbar.jsx
+++ src/content/Navbar/Navbar.jsx
@@ -3,10 +3,11 @@ export default function Navbar() {
     return (
         <div className="navbar">
             <div className="navbarTop">
+                <div className='navbarLogo'></div>
             </div>
             <div className="navbarLeft">
                 <nav>
-                    <ul>
+                    <ul className='navbarOptions'>
                         <li><Svg type={'chart-bar'}      height={16} fill={"#00D954"}/>Dashboard</li>
                         <li><Svg type={'shovel'}         height={16} fill={"#5A5773"}/>Leads</li>
                         <li><Svg type={'bullseye-arrow'} height={16} fill={"#5A5773"}/>Campanhas</li>
@@ -16,7 +17,7 @@ export default function Navbar() {
                         <li><Svg type={'cog'}            height={16} fill={"#5A5773"}/>Configurações</li>
                     </ul>
                 </nav>
-                    <ul>
+                    <ul className='navbarSignOut'>
                         <li><Svg type={'sign-out-alt'} height={16} fill={"#5A5773"}/>Sair</li>
                     </ul>
             </div>
