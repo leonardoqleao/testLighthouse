b40a0296c4bb4bc3254da4d230a042916eff0dff
leonardo q <leonardoq.leao@outlook.com>
2022-05-14 22:54:49 -0300

    update: alter description

--- src/content/Navbar/Navbar.jsx
+++ src/content/Navbar/Navbar.jsx
@@ -1,21 +1,21 @@
+import Svg from '../../svg'
 export default function Navbar() {
     return (
         <div className="Navbar">
             <div className="navbarLeft">
                 <nav>
                     <ul>
-                        <li>Dashboard</li>
-                        <li>Leads</li>
-                        <li>Campanhas</li>
-                        <li>Vendas</li>
-                        <li>Clientes</li>
-                        <li>Cobrança</li>
-                        <li>Configurações</li>
+                        <li><Svg type={'chart-bar'}      height={16} fill={"#00D954"}/>Dashboard</li>
+                        <li><Svg type={'shovel'}         height={16} fill={"#5A5773"}/>Leads</li>
+                        <li><Svg type={'bullseye-arrow'} height={16} fill={"#5A5773"}/>Campanhas</li>
+                        <li><Svg type={'shopping-cart'}  height={16} fill={"#5A5773"}/>Vendas</li>
+                        <li><Svg type={'user'}           height={16} fill={"#5A5773"}/>Clientes</li>
+                        <li><Svg type={'receipt'}        height={16} fill={"#5A5773"}/>Cobrança</li>
+                        <li><Svg className='teste' type={'cog'}            height={16} fill={"#5A5773"}/>Configurações</li>
                     </ul>
                 </nav>
             </div>
             <div className="navbarTop">
-
             </div>
         </div>
     )
