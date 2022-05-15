646e96a86bf436e92179c030c77b89957fc02093
leonardo q <leonardoq.leao@outlook.com>
2022-05-15 14:05:10 -0300

    feat: update hook name Svg > Imgs

--- src/content/Navbar/Navbar.jsx
+++ src/content/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import Svg from '../../svg'
+import Imgs from '../../imgs'
 export default function Navbar() {
     return (
         <div className="navbar">
@@ -8,17 +8,17 @@ export default function Navbar() {
             <div className="navbarLeft">
                 <nav>
                     <ul className='navbarOptions'>
-                        <li><Svg type={'chart-bar'}      height={16} fill={"#00D954"}/>Dashboard</li>
-                        <li><Svg type={'shovel'}         height={16} fill={"#5A5773"}/>Leads</li>
-                        <li><Svg type={'bullseye-arrow'} height={16} fill={"#5A5773"}/>Campanhas</li>
-                        <li><Svg type={'shopping-cart'}  height={16} fill={"#5A5773"}/>Vendas</li>
-                        <li><Svg type={'user'}           height={16} fill={"#5A5773"}/>Clientes</li>
-                        <li><Svg type={'receipt'}        height={16} fill={"#5A5773"}/>Cobrança</li>
-                        <li><Svg type={'cog'}            height={16} fill={"#5A5773"}/>Configurações</li>
+                        <li><Imgs type={'chart-bar'}      className={'svgClass'} height={16} fill={"#00D954"}/>Dashboard</li>
+                        <li><Imgs type={'shovel'}         className={'svgClass'} height={16} fill={"#5A5773"}/>Leads</li>
+                        <li><Imgs type={'bullseye-arrow'} className={'svgClass'} height={16} fill={"#5A5773"}/>Campanhas</li>
+                        <li><Imgs type={'shopping-cart'}  className={'svgClass'} height={16} fill={"#5A5773"}/>Vendas</li>
+                        <li><Imgs type={'user'}           className={'svgClass'} height={16} fill={"#5A5773"}/>Clientes</li>
+                        <li><Imgs type={'receipt'}        className={'svgClass'} height={16} fill={"#5A5773"}/>Cobrança</li>
+                        <li><Imgs type={'cog'}            className={'svgClass'} height={16} fill={"#5A5773"}/>Configurações</li>
                     </ul>
                 </nav>
                     <ul className='navbarSignOut'>
-                        <li><Svg type={'sign-out-alt'} height={16} fill={"#5A5773"}/>Sair</li>
+                        <li><Imgs type={'sign-out-alt'} className={'svgClass'} height={16} fill={"#5A5773"}/>Sair</li>
                     </ul>
             </div>
         </div>
