05030be58c2aa467b022d491d655a2be15bb47e9
leonardo q <leonardoq.leao@outlook.com>
2022-05-16 15:20:16 -0300

    feat: create navbarContext

---
+++ src/context/NavbarContext.jsx
@@ -0,0 +1,15 @@
+import React, { useState , createContext } from "react";
+
+export const OptionsNavbarContext = createContext({
+    selected: '',
+    setSelected: ()=>{}
+});
+export const OptionsNavbarProvider = ({ children }) => {
+
+    const [selected, setSelected] = useState({Dashboard: 'selected'})
+    return (
+        <OptionsNavbarContext.Provider value={{selected, setSelected}}>
+            {children}
+        </OptionsNavbarContext.Provider>
+    )
+}
---
+++ src/context/index.js
@@ -0,0 +1,1 @@
+export { OptionsNavbarProvider, OptionsNavbarContext } from './NavbarContext.jsx'
