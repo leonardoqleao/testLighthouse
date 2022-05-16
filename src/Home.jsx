ac0493d2890ebe7e3b4ea8a1c9d87b6df766195a
leonardo q <leonardoq.leao@outlook.com>
2022-05-16 18:43:41 -0300

    feat: add routes in Home

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,12 +1,22 @@
 import { Navbar, Dashboard } from "./content"
 import { OptionsNavbarProvider } from "./context"
+import { Route, Routes } from "react-router-dom";
+
 export default function Home() {
   return (
     <div className="Home">
       <OptionsNavbarProvider>
         <Navbar />
       </OptionsNavbarProvider>
-      <Dashboard />
+      <Routes>
+        <Route exact path='/' element={<Dashboard />} />
+        <Route path='/leads' element={<div>Leads Empty</div>} />
+        <Route path='/campanhas' element={<div>Campanhas Empty</div>} />
+        <Route path='/vendas' element={<div>Vendas Empty</div>} />
+        <Route path='/clientes' element={<div>Clientes Empty</div>} />
+        <Route path='/cobranca' element={<div>Cobranca Empty</div>} />
+        <Route path='/configuracoes' element={<div>Configurações Empty</div>} />
+      </Routes>
     </div>
   )
 }
