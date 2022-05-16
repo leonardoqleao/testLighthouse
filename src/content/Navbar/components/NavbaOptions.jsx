433befc764843227c40daf99d0246dcaff4a0341
leonardo q <leonardoq.leao@outlook.com>
2022-05-16 18:47:23 -0300

    feat: add link to routes

--- src/content/Navbar/components/NavbaOptions.jsx
+++ src/content/Navbar/components/NavbaOptions.jsx
@@ -1,4 +1,5 @@
 import { useContext } from "react"
+import { Link } from "react-router-dom"
 import { OptionsNavbarContext } from "../../../context"
 import Imgs from "../../../imgs"
 
@@ -31,79 +32,97 @@ export default function NavbarOptions() {
 function Dashboard() {
     const { selected, setSelected } = useContext(OptionsNavbarContext)
     return (
-        <li onClick={(e) => { if (!e.target.className) setSelected({ Dashboard: 'selected' }) }} className={selected.Dashboard}>
-            <span className='svgClass'>
-                <Imgs type={'chart-bar'} height={16} />
-            </span>
-            <span>Dashboard</span>
-        </li>
+        <Link to={'/'}>
+            <li onClick={(e) => { if (!e.target.className) setSelected({ Dashboard: 'selected' }) }} className={selected.Dashboard}>
+                <span className='svgClass'>
+                    <Imgs type={'chart-bar'} height={16} />
+                </span>
+                <span>Dashboard</span>
+            </li>
+        </Link>
     )
 }
 function Leads() {
     const { selected, setSelected } = useContext(OptionsNavbarContext)
     return (
-        <li onClick={(e) => { if (!e.target.className) setSelected({ Leads: 'selected' }) }} className={selected.Leads}>
-            <span className='svgClass'>
-                <Imgs type={'shovel'} height={16} />
-            </span>
-            <span>Leads</span>
-        </li>
+        <Link to={'/leads'}>
+            <li onClick={(e) => { if (!e.target.className) setSelected({ Leads: 'selected' }) }} className={selected.Leads}>
+                <span className='svgClass'>
+                    <Imgs type={'shovel'} height={16} />
+                </span>
+                <span>Leads</span>
+            </li>
+        </Link>
     )
 }
 function Campanhas() {
     const { selected, setSelected } = useContext(OptionsNavbarContext)
     return (
-        <li onClick={(e) => { if (!e.target.className) setSelected({ Campanhas: 'selected' }) }} className={selected.Campanhas} >
-            <span className='svgClass'>
-                <Imgs type={'bullseye-arrow'} height={16} />
-            </span>
-            <span>
-                Campanhas
-            </span>
-        </li>
+        <Link to={'/campanhas'}>
+            <li onClick={(e) => { if (!e.target.className) setSelected({ Campanhas: 'selected' }) }} className={selected.Campanhas} >
+                <span className='svgClass'>
+                    <Imgs type={'bullseye-arrow'} height={16} />
+                </span>
+                <span>
+                    Campanhas
+                </span>
+            </li>
+        </Link>
     )
 }
 function Vendas() {
     const { selected, setSelected } = useContext(OptionsNavbarContext)
     return (
-        <li onClick={(e) => { if (!e.target.className) setSelected({ Vendas: 'selected' }) }} className={selected.Vendas} >
-            <span className='svgClass'>
-                <Imgs type={'shopping-cart'} height={16} />
-            </span>
-            <span>Vendas</span>
-        </li>
+        <Link to={'/vendas'}>
+
+            <li onClick={(e) => { if (!e.target.className) setSelected({ Vendas: 'selected' }) }} className={selected.Vendas} >
+                <span className='svgClass'>
+                    <Imgs type={'shopping-cart'} height={16} />
+                </span>
+                <span>Vendas</span>
+            </li>
+        </Link>
     )
 }
 function Clientes() {
     const { selected, setSelected } = useContext(OptionsNavbarContext)
     return (
-        <li onClick={(e) => { if (!e.target.className) setSelected({ Clientes: 'selected' }) }} className={selected.Clientes} >
-            <span className='svgClass'>
-                <Imgs type={'user'} height={16} />
-            </span>
-            <span>Clientes</span>
-        </li>
+        <Link to={'/clientes'}>
+
+            <li onClick={(e) => { if (!e.target.className) setSelected({ Clientes: 'selected' }) }} className={selected.Clientes} >
+                <span className='svgClass'>
+                    <Imgs type={'user'} height={16} />
+                </span>
+                <span>Clientes</span>
+            </li>
+        </Link>
     )
 }
 function Cobranca() {
     const { selected, setSelected } = useContext(OptionsNavbarContext)
     return (
-        <li onClick={(e) => { if (!e.target.className) setSelected({ Cobranca: 'selected' }) }} className={selected.Cobranca} >
-            <span className='svgClass'>
-                <Imgs type={'receipt'} height={16} />
-            </span>
-            <span>Cobrança</span>
-        </li>
+        <Link to={'/cobranca'}>
+
+            <li onClick={(e) => { if (!e.target.className) setSelected({ Cobranca: 'selected' }) }} className={selected.Cobranca} >
+                <span className='svgClass'>
+                    <Imgs type={'receipt'} height={16} />
+                </span>
+                <span>Cobrança</span>
+            </li>
+        </Link>
     )
 }
 function Configuracoes() {
     const { selected, setSelected } = useContext(OptionsNavbarContext)
     return (
-        <li onClick={(e) => { if (!e.target.className) setSelected({ Configuracoes: 'selected' }) }} className={selected.Configuracoes} >
-            <span className='svgClass'>
-                <Imgs type={'cog'} height={16} />
-            </span>
-            <span>Configurações</span>
-        </li>
+        <Link to={'/configuracoes'}>
+
+            <li onClick={(e) => { if (!e.target.className) setSelected({ Configuracoes: 'selected' }) }} className={selected.Configuracoes} >
+                <span className='svgClass'>
+                    <Imgs type={'cog'} height={16} />
+                </span>
+                <span>Configurações</span>
+            </li>
+        </Link>
     )
 }
