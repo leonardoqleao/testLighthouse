19ce85c42291017cd5abe7305a8ee1ae8861d137
leonardo q <leonardoq.leao@outlook.com>
2022-05-16 18:43:15 -0300

    feat: add react-router-dom in index app

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './css';
-import Home  from './Home.jsx';
+import Home from './Home.jsx';
+import { BrowserRouter as Router } from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router>
+      <Home />
+    </Router>
   </React.StrictMode>,
   document.getElementById("main")
 )
