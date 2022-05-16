f15ed3b542fb64fe4d058b445b90b6a843522651
leonardo q <leonardoq.leao@outlook.com>
2022-05-16 12:13:33 -0300

    feat: add logo in navbartop

--- src/content/Navbar/Navbar.jsx
+++ src/content/Navbar/Navbar.jsx
@@ -1,10 +1,20 @@
+import Imgs from '../../imgs'
 import { NavbarOptions } from './components'
 
 export default function Navbar() {
     return (
         <div className="navbar">
             <div className="navbarTop">
-                <div className='navbarLogo'></div>
+                <div className='navbarLogo'>
+                    <div className='container'>
+                        <div>
+                            <Imgs type={'logo'} className={'logo'} />
+                        </div>
+                        <div>
+                            <Imgs type={'logoname'} className={'logoname'} />
+                        </div>
+                    </div>
+                </div>
             </div>
             <NavbarOptions />
         </div>
