29c96baee5fe127a9386e37ac85aa1d14618bcaf
leonardo q <leonardoq.leao@outlook.com>
2022-05-17 19:05:40 -0300

    feat: add notification information in bell and comment icons

--- src/content/Navbar/components/NavbarTop.jsx
+++ src/content/Navbar/components/NavbarTop.jsx
@@ -1,6 +1,27 @@
+import { Component, useEffect, useState } from 'react'
 import Imgs from '../../../imgs'
 
 export const NavbarTop = () => {
+    const [infNotifyComment, setInfNotifyComment] = useState(3);
+    const [infNotifyBell, setInfNotifyBell] = useState(1);
+    const [displayInfNotifyBell, setDisplayInfNotifyBell] = useState('');
+    const [displayInfNotifyComment, setDisplayInfNotifyComment] = useState('');
+
+    useEffect(() => {
+        if (infNotifyBell <= 0) {
+            setDisplayInfNotifyBell('none')
+            setTimeout(() => { setInfNotifyBell(1) }, 2000)
+        } else {
+            setDisplayInfNotifyBell('')
+        }
+        if (infNotifyComment <= 0) {
+            setDisplayInfNotifyComment('none')
+            setTimeout(() => { setInfNotifyComment(3) }, 2000)
+        } else {
+            setDisplayInfNotifyComment('')
+        }
+
+    }, [infNotifyComment, infNotifyBell])
     return (
         <nav className="navbarTop">
             <div className='navbarLogo'>
@@ -20,14 +41,16 @@ export const NavbarTop = () => {
                     </span>
                 </div>
                 <div>
-                    <span>
-                        <Imgs className={'comment'} type={'comment'} height={30} />
+                    <span onClick={() => setInfNotifyComment(0)} className='comment'>
+                        <span style={{ display: displayInfNotifyComment }} className='notificationNumbers'>{infNotifyComment}</span>
+                        <Imgs type={'comment'} height={30} />
+                    </span>
+                    <span onClick={() => setInfNotifyBell(0)} className={'bell'}>
+                        <span style={{ display: displayInfNotifyBell }} className='notificationNumbers'>{infNotifyBell}</span>
+                        <Imgs type={'bell'} height={30} />
                     </span>
                     <span>
-                        <Imgs className={'bell'} type={'bell'} height={30} />
-                    </span>
-                    <span>
-                        <Imgs className={'myPicture'} type={'mypicutre'}/>
+                        <Imgs className={'myPicture'} type={'mypicutre'} />
                     </span>
                 </div>
             </div>
