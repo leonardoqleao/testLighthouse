0aa6c287abc670cfb0242bd61b0f2d27d5e6f0bb
leonardo q <leonardoq.leao@outlook.com>
2022-05-17 16:41:47 -0300

    feat: add svg icons & picture in navbar top

--- src/content/Navbar/components/NavbarTop.jsx
+++ src/content/Navbar/components/NavbarTop.jsx
@@ -2,17 +2,35 @@ import Imgs from '../../../imgs'
 
 export const NavbarTop = () => {
     return (
-        <div className="navbarTop">
+        <nav className="navbarTop">
             <div className='navbarLogo'>
                 <div className='container'>
-                    <div>
+                    <span>
                         <Imgs type={'logo'} className={'logo'} />
-                    </div>
+                    </span>
                     <div className='logoname'>
                         <Imgs type={'logoname'} />
                     </div>
                 </div>
             </div>
-        </div>
+            <div className='navbarTop2'>
+                <div className='container'>
+                    <span>
+                        <Imgs className={'bars'} type={'bars'} height={30} />
+                    </span>
+                </div>
+                <div>
+                    <span>
+                        <Imgs className={'comment'} type={'comment'} height={30} />
+                    </span>
+                    <span>
+                        <Imgs className={'bell'} type={'bell'} height={30} />
+                    </span>
+                    <span>
+                        <Imgs className={'myPicture'} type={'mypicutre'}/>
+                    </span>
+                </div>
+            </div>
+        </nav>
     )
 }
