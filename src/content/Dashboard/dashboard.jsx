4aa9773c34617147a7964091001798ef46f2a8cb
leonardo q <leonardoq.leao@outlook.com>
2022-05-18 14:58:57 -0300

    style: style in propeti conversao

--- src/content/Dashboard/dashboard.jsx
+++ src/content/Dashboard/dashboard.jsx
@@ -12,41 +12,53 @@ export default function Dashboard() {
                 </h1>
                 <div className="container">
                     <p>
-                       <b>Período:</b>Últimos 12 meses
+                        <b>Período:</b>Últimos 12 meses
                     </p>
                     <div className="filter">
-                        <Imgs type={"filter"} height={16}/>
+                        <Imgs type={"filter"} height={16} />
                         <p>filter</p>
                     </div>
                 </div>
             </div>
             <div className="properties">
-                <div className="vendas">
+                <div className="propertiBox vendas">
                     <div className="titleProperties">
-                        <Imgs type={'shopping-cart'} height={20}/>
+                        <Imgs type={'shopping-cart'} height={20} />
                         <p>Vendas</p>
                     </div>
                     <p className="valueProperties">245</p>
                     <span className='statusProperties'>
-                        <Imgs type={'sort-up'} height={20}/>
+                        <Imgs type={'sort-up'} height={20} />
                         12,67%
                     </span>
                 </div>
-                <div className="faturamento">
-                <div className="titleProperties">
-                        <Imgs type={'dollar-sign'} height={20}/>
+                <div className="propertiBox faturamento">
+                    <div className="titleProperties">
+                        <Imgs type={'dollar-sign'} height={20} />
                         <p>Faturamento</p>
                     </div>
                     <p className="valueProperties">
                         <p>R$</p>12.167.943
                     </p>
                     <span className='statusProperties'>
-                        <Imgs type={'sort-up'} height={20}/>
+                        <Imgs type={'sort-up'} height={20} />
                         4,67%
                     </span>
                 </div>
-                <div className="conversao"></div>
-                <div className="leads"></div>
+                <div className="propertiBox conversao">
+                    <div className="titleProperties">
+                        <Imgs type={'exchange-alt'} height={16} />
+                        <p>Conversão</p>
+                    </div>
+                    <p className="valueProperties">
+                        12%
+                    </p>
+                    <span className='statusProperties'>
+                        <Imgs type={'sort-up'} height={20} />
+                        21,67%
+                    </span>
+                </div>
+                <div className="propertiBox leads"></div>
             </div>
             <div className="grafic"></div>
         </div>
