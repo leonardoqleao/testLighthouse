674443abdf3475870b52bd168829515771638e41
leonardo q <leonardoq.leao@outlook.com>
2022-05-18 12:23:11 -0300

    feat: add title content

--- src/content/Dashboard/dashboard.jsx
+++ src/content/Dashboard/dashboard.jsx
@@ -1,10 +1,25 @@
 import { useContext } from "react"
 import { OptionsStyleContext } from '../../context'
+import Imgs from "../../imgs"
+
 export default function Dashboard() {
     const { style } = useContext(OptionsStyleContext)
     return (
         <div className={style.dashboard}>
-            <div className="title"></div>
+            <div className="title">
+                <h1>
+                    Indicadores
+                </h1>
+                <div>
+                    <p>
+                        Período: Últimos 12 meses
+                    </p>
+                    <div className="filter">
+                        <Imgs type={"filter"} height={16}/>
+                        <p>filter</p>
+                    </div>
+                </div>
+            </div>
             <div className="properties"></div>
             <div className="grafic"></div>
         </div>
