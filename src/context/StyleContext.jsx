7d93887fab5f47b6ab12ab9ca25d1ecbe157fd17
leonardo q <leonardoq.leao@outlook.com>
2022-05-18 11:42:58 -0300

    feat: add navbartop in styleContext

--- src/context/StyleContext.jsx
+++ src/context/StyleContext.jsx
@@ -1,22 +1,23 @@
-import { useState , createContext } from "react";
+import { useState, createContext } from "react";
 
 const styleClass = {
     navbarLeft: 'navbarLeft navbarLeftClose',
     navbarOptions: 'navbarOptions navbarOptionsClose',
     navbarSignOut: 'navbarSignOut navbarSignOutClose',
     dashboard: 'dashboard dashboardW',
-    navbarLogo: 'navbarLogo navbarLogoW'
+    navbarLogo: 'navbarLogo navbarLogoW',
+    navbarTop2: 'navbarTop2 navbarTop2w'
 }
 
 export const OptionsStyleContext = createContext({
     style: {},
-    setStyle: ()=>{}
+    setStyle: () => { }
 });
 export const OptionsStyleProvider = ({ children }) => {
 
     const [style, setStyle] = useState(styleClass)
     return (
-        <OptionsStyleContext.Provider value={{style, setStyle}}>
+        <OptionsStyleContext.Provider value={{ style, setStyle }}>
             {children}
         </OptionsStyleContext.Provider>
     )
