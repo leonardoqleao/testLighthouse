0c88fbb036adbb63ed681c34f8178ad6dccef89c
leonardo q <leonardoq.leao@outlook.com>
2022-05-18 10:21:18 -0300

    feat: add styleContext in dashboard

--- src/content/Dashboard/dashboard.jsx
+++ src/content/Dashboard/dashboard.jsx
@@ -1,6 +1,9 @@
+import { useContext } from "react"
+import { OptionsStyleContext } from '../../context'
 export default function Dashboard (){
+    const { style } = useContext(OptionsStyleContext)
     return (
-        <div className="dashboard">
+        <div className={style.dashboard}>
             bom diass
         </div>
     )
